5940b1291484b3b0469f216380689f3721070849
Kesavan D <kesavand@syncfusion.com>
2019-06-27 17:49:01 +0530

    Added EJ2 Document Editor Web services project.

---
+++ src/Models/DiagramData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EJ2APIServices.Models
+{
+    public partial class DiagramData
+    {
+        public int Id { get; set; }
+        public string DiagramName { get; set; }
+        public string DiagramContent { get; set; }
+    }
+}
---
+++ src/Startup.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.Edm;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.OData.UriParser;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using EJ2APIServices.Models;
+using EJ2APIServices.Controllers;
+
+
+namespace EJ2APIServices
+{
+    public class Startup
+    {
+        private string _contentRootPath = "";
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+            _contentRootPath = env.ContentRootPath;
+            ServerPath.MapPath = _contentRootPath;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOData();
+            services.AddMvc()
+.AddJsonOptions(x =>
+{
+    x.SerializerSettings.ContractResolver = null;
+});
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+                });
+            });
+            string connection = Configuration.GetConnectionString("Test");
+            if (connection.Contains("%CONTENTROOTPATH%"))
+            {
+                connection = connection.Replace("%CONTENTROOTPATH%", _contentRootPath);
+
+            }
+            services.AddEntityFrameworkNpgsql().AddDbContext<EEJ2SERVICEEJ2WEBSERVICESSRCAPP_DATADIAGRAMMDFContext>(options => options.UseNpgsql(connection));
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseCors("AllowAllOrigins");
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc(b =>
+            {
+                b.Count().Filter().OrderBy().Expand().Select().MaxTop(null);
+            });
+
+        }
+
+    }
+
+    public class ServerPath
+    {
+        public static string MapPath = "";
+    }
+}
