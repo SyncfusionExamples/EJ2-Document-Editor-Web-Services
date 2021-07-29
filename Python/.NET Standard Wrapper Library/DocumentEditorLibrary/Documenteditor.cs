cee9a89c2408b10c5e1573089d71afbbe36f1579
Suriya.Murugan <suriya.murugan@syncfusion.com>
2021-07-29 17:59:42 +0530

    Integrate dotnet library in python

---
+++ Python/.NET Standard Wrapper Library/DocumentEditorLibrary/Documenteditor.cs
@@ -0,0 +1,94 @@
+﻿using Syncfusion.EJ2.DocumentEditor;
+using System;
+using System.IO;
+
+namespace DocumentEditorLibrary
+{
+    public class Documenteditor
+    {
+        public string Import(string base64String)
+        {
+
+            byte[] byteArray = Convert.FromBase64String(base64String);
+            Stream stream = new MemoryStream(byteArray);
+
+
+            try
+            {
+                //Stream stream = System.IO.File.Open(path, FileMode.Open, FileAccess.ReadWrite);
+                WordDocument document = WordDocument.Load(stream, FormatType.Docx); ;
+                string json = Newtonsoft.Json.JsonConvert.SerializeObject(document);
+                document.Dispose();
+                stream.Dispose();
+                return json;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public string SystemClipboard(string Content, string type)
+        {
+            if (Content != null && Content != "")
+            {
+                try
+                {
+
+                    WordDocument document = WordDocument.LoadString(Content, GetFormatType(type));
+                    string json = Newtonsoft.Json.JsonConvert.SerializeObject(document);
+                    document.Dispose();
+                    return json;
+                }
+                catch (Exception ex)
+                {
+                    return ex.Message;
+                }
+            }
+            return "";
+        }
+        public string ExportSFDT(string content)
+        {
+            Stream document = WordDocument.Save(content, FormatType.Docx);
+            FileStream file = new FileStream("sample.docx", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            document.CopyTo(file);
+            file.Close();
+            document.Close();
+            return "success";
+        }
+
+        public string[] RestrictEditing(string passwordBase64, string saltBase64, int spinCount)
+        {
+            if (passwordBase64 == "" && passwordBase64 == null)
+                return null;
+            return WordDocument.ComputeHash(passwordBase64, saltBase64, spinCount);
+        }
+
+        internal static FormatType GetFormatType(string format)
+        {
+            if (string.IsNullOrEmpty(format))
+                throw new NotSupportedException("EJ2 DocumentEditor does not support this file format.");
+            switch (format.ToLower())
+            {
+                case ".dotx":
+                case ".docx":
+                case ".docm":
+                case ".dotm":
+                    return FormatType.Docx;
+                case ".dot":
+                case ".doc":
+                    return FormatType.Doc;
+                case ".rtf":
+                    return FormatType.Rtf;
+                case ".txt":
+                    return FormatType.Txt;
+                case ".xml":
+                    return FormatType.WordML;
+                case ".html":
+                    return FormatType.Html;
+                default:
+                    throw new NotSupportedException("EJ2 DocumentEditor does not support this file format.");
+            }
+        }
+    }
+}
---
+++ Python/README.md
@@ -0,0 +1,20 @@
+# Integrated .Net Core Library in python
+
+We have created Documenteditor utility with following functionalities:
+
+* Import
+* ExportSfdt
+* SystemClipboard
+* RestrictEditing
+
+## Import
+
+You can provide parameter as base64String of file which will convert to SFDT format by using this API. In import.py, we have provided default document as base64String.
+
+## SystemClipboard
+
+You can make use of this service in order to paste system clipboard data by preserving the formatting. In parameter, you can provide clipboard content as rtf or html string with type.
+
+## ExportSfdt
+
+You can export the SFDT string Doc, DOCX, RTF, Txt, WordML, HTML formats by using this API in server side.
