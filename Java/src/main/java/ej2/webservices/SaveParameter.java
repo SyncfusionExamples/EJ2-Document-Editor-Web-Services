1b2cf271b2da1bef153b5ae923efd533cae4e21a
Karthikk15 <karthikkyuvi@gmail.com>
2021-09-03 17:22:22 +0530

    Included Export Web API actions in .Net / Java

---
+++ Java/src/main/java/ej2/webservices/SaveParameter.java
@@ -0,0 +1,24 @@
+package ej2.webservices;
+
+public class SaveParameter {
+	private String _content;
+	private String _fileName;
+
+	public String getContent() {
+		return _content;
+	}
+
+	public String setContent(String value) {
+		_content = value;
+		return value;
+	}
+
+	public String getFileName() {
+		return _fileName;
+	}
+
+	public String setFileName(String value) {
+		_fileName = value;
+		return value;
+	}
+}
