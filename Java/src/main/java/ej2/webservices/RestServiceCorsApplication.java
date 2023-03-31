d3f881b1c4b18a9105ef09f92ec16554f2237bd5
Thamizhselvanv <thamizhselvan.v@syncfusion.com>
2023-03-31 16:37:24 +0530

    Configured the MultipartFile upload and request size limit.

--- Java/src/main/java/ej2/webservices/RestServiceCorsApplication.java
+++ Java/src/main/java/ej2/webservices/RestServiceCorsApplication.java
@@ -1,12 +1,16 @@
 package ej2.webservices;
 import java.util.Collections;
 
+import javax.servlet.MultipartConfigElement;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.util.unit.DataSize;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 // Refer the licensing package
 import com.syncfusion.licensing.*;
@@ -26,6 +30,14 @@ public class RestServiceCorsApplication extends SpringBootServletInitializer {
 		SpringApplication app = new SpringApplication(RestServiceCorsApplication.class);
 		app.setDefaultProperties(Collections.singletonMap("server.port", "9090"));
 		app.run(args);
+	}
+	
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+	    MultipartConfigFactory factory = new MultipartConfigFactory();
+	    factory.setMaxFileSize(DataSize.ofBytes(1073741824));
+	    factory.setMaxRequestSize(DataSize.ofBytes(1073741824));
+	    return factory.createMultipartConfig();
 	}
 
 	// @Bean
