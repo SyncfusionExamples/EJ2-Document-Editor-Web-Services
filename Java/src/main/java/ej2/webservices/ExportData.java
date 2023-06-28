8bdf720f675cfce6a535778bd597a775c2d40f06
Kavitha Muralitharan <98306850+KAVITHAMURALITHARAN@users.noreply.github.com>
2023-06-28 14:44:41 +0530

    Create ExportData.java

---
+++ Java/src/main/java/ej2/webservices/ExportData.java
@@ -0,0 +1,14 @@
+package ej2.webservices;
+
+public class ExportData {
+    private String _documentData;
+
+    public String getDocumentData() {
+        return _documentData;
+    }
+
+    public String setDocumentData(String value) {
+        _documentData = value;
+        return value;
+    }
+}
