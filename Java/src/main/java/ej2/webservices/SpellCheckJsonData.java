2d55d7e346486a9fec083d2c16219695e9643374
manomurugan <mano.murugan@syncfusion.com>
2025-11-27 14:23:57 +0530

    952237: Changes the spellcheck parameter like in ASPNET

--- Java/src/main/java/ej2/webservices/SpellCheckJsonData.java
+++ Java/src/main/java/ej2/webservices/SpellCheckJsonData.java
@@ -4,15 +4,45 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class SpellCheckJsonData
 {
- @JsonProperty("LanguageID")
- int languageID;
- @JsonProperty("TexttoCheck")
- String texttoCheck;
- @JsonProperty("CheckSpelling")
- boolean checkSpelling;
- @JsonProperty("CheckSuggestion")
- boolean checkSuggestion;
- @JsonProperty("AddWord")
- boolean addWord;
+    @JsonProperty("LanguageID")
+    int languageID;
 
+    @JsonProperty("TexttoCheck")
+    String texttoCheck;
+
+    @JsonProperty("CheckSpelling")
+    boolean checkSpelling;
+
+    @JsonProperty("CheckSuggestion")
+    boolean checkSuggestion;
+
+    @JsonProperty("AddWord")
+    boolean addWord;
+
+    @JsonProperty("IgnoreUppercase")
+    boolean ignoreUppercase;
+
+    @JsonProperty("UserName")
+    String userName;
+
+    public int getLanguageID() { return languageID; }
+    public void setLanguageID(int languageID) { this.languageID = languageID; }
+
+    public String getTexttoCheck() { return texttoCheck; }
+    public void setTexttoCheck(String texttoCheck) { this.texttoCheck = texttoCheck; }
+
+    public boolean isCheckSpelling() { return checkSpelling; }
+    public void setCheckSpelling(boolean checkSpelling) { this.checkSpelling = checkSpelling; }
+
+    public boolean isCheckSuggestion() { return checkSuggestion; }
+    public void setCheckSuggestion(boolean checkSuggestion) { this.checkSuggestion = checkSuggestion; }
+
+    public boolean isAddWord() { return addWord; }
+    public void setAddWord(boolean addWord) { this.addWord = addWord; }
+
+    public boolean isIgnoreUppercase() { return ignoreUppercase; }
+    public void setIgnoreUppercase(boolean ignoreUppercase) { this.ignoreUppercase = ignoreUppercase; }
+
+    public String getUserName() { return userName; }
+    public void setUserName(String userName) { this.userName = userName; }
 }
